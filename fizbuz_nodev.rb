1193f3b167a0e35171ba186a7ad34510e31cdc85
FFFYK <fukuma21@gmail.com>
2015-04-10 00:16:30 +0900

    method refactoring

--- fizbuz_nodev.rb
+++ fizbuz_nodev.rb
@@ -4,44 +4,21 @@ init = 0
 max  = ARGV[0].to_i
 
 #予め答えを生成
-#3の倍数を格納
-def multiple_3(max)
+#最大値までの基準値の倍数を返す
+def multiple_calc(multi, max)
   score_result = []
-  score = 3
+  score = multi
 
   while (max >= score)
     score_result.push(score)
-    score += 3
+    score += multi
   end
   return score_result
 end
 
-#5の倍数を格納
-def multiple_5(max)
-  score_result = []
-  score = 5
-  while (max >= score)
-    score_result.push(score)
-    score += 5
-  end
-  return score_result
-end
-
-#15の倍数を格納
-def multiple_15(max)
-  score_result = []
-  score = 15
-
-  while (max >= score)
-    score_result.push(score)
-    score += 15
-  end
-  return score_result
-end
-
-score_15 = multiple_15(max)
-score_5  = multiple_5(max)
-score_3  = multiple_3(max)
+score_15 = multiple_calc(15, max)
+score_5  = multiple_calc(5, max)
+score_3  = multiple_calc(3, max)
 
 max +=1
 max.times do |numbers|
